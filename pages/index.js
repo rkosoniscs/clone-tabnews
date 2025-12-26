4b05bf1950d9925e493687ee080ede6a1f1410f5
rkosoniscs <68347623+rkosoniscs@users.noreply.github.com>
2025-12-26 19:03:57 +0000

    adiciona scritps `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-function Home(){
-    let html = <h1>Caneta azul, azul caneta. Caneta Azul tá marcada com minha letra...</h1>
-    return html
+function Home() {
+  let html = (
+    <h1>Caneta azul, azul caneta. Caneta Azul tá marcada com minha letra...</h1>
+  );
+  return html;
+}
+
+function test2() {
+  console.log("Teste");
 }
 
 export default Home;
